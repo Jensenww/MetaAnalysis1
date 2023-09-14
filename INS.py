0db0c62a8f05765e51dbc3cba666ca3f43a579bf
jensen <lidaw@uci.edu>
2023-09-14 16:25:18 -0700

    new

--- INS.py
+++ INS.py
@@ -1,6 +1,3 @@
-git config--global user.email "lidaw@uci.edu"
-git config--global user.name "Jensen"
-
 
 import pandas as pd
 df = pd.read_csv(r'C:\Users\jense\OneDrive\Desktop\Meta数据分析\IG_market_dataset.csv')
