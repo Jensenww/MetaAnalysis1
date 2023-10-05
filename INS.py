834b01f92e9f68ed5617680fb4ec40c84c15a9bc
jensen <lidaw@uci.edu>
2023-10-05 00:44:04 -0400

    new

--- INS.py
+++ INS.py
@@ -1,4 +1,6 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
 import pandas as pd
 pd.set_option('display.float_format', '{:.2f}'.format)
 df1 = pd.read_csv(r'C:\Users\jense\OneDrive\Desktop\Meta数据分析\IG_market_dataset.csv')
@@ -6,6 +8,7 @@ df2 = pd.read_csv(r'C:\Users\jense\OneDrive\Desktop\Meta数据分析\ER.csv')
 df3 = pd.read_csv(r'C:\Users\jense\OneDrive\Desktop\Meta数据分析\country_stats.csv')
 # Convert strings with commas to floats
 df3['area_km2'] = df3['area_km2'].str.replace(',', '').astype(float)
+
 
 df3[['GDP','currency code']] = df3.gdp_local_currency_trillion.str.split(expand=True)
 df3[['gdp_per_capital','currency code']] =df3.gdp_per_capita_local_currency.str.split(expand=True)
@@ -15,8 +18,9 @@ result['GDP'] = pd.to_numeric(result['GDP'], errors='coerce')
 result['gdp_per_capital'] = pd.to_numeric(result['gdp_per_capital'], errors='coerce')
 result['exchange_rate_to_usd'] = pd.to_numeric(result['exchange_rate_to_usd'], errors='coerce')
 
-result['GDP_USD'] = result['GDP'] * result['exchange_rate_to_usd']
-result['GDP_per_capita_USD'] = result['gdp_per_capital'] / result['exchange_rate_to_usd']
+result['GDP_USD'] = (result['GDP'] / result['exchange_rate_to_usd'])
+result['GDP_per_capita_USD'] = (result['gdp_per_capital'] / result['exchange_rate_to_usd'])
+
 
 selected_columns = result[['country_region','GDP','gdp_per_capital','currency code','GDP_USD','GDP_per_capita_USD']]
 print(selected_columns)
@@ -100,11 +104,115 @@ print("the correlation between users  and hdi is:", correlation17)
 correlation18 = new_table['users_million'].corr(new_table['population_density_km2'])
 print("the correlation between users and population_density_km2 is:", correlation18)
 
+X = new_table[['GDP_USD', 'GDP_per_capita_USD', 'hdi', 'population_density_km2','area_km2','population_million']]
+y = new_table[['margin_bn_usd']]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+print (X_train.shape)
+print (y_train.shape)
+print (X_test.shape)
+print (y_test.shape)
+
+from sklearn.linear_model import LinearRegression
+linreg = LinearRegression()
+linreg.fit(X_train, y_train)
+print (linreg.intercept_)
+
+
+coef_list = [float(coef) for coef in linreg.coef_.flatten()]
+formatted_coef_list = ['{:.10f}'.format(coef) for coef in coef_list]
+print(formatted_coef_list)
+
+
+#模型拟合测试集
+y_pred = linreg.predict(X_test)
+from sklearn import metrics
+# 用scikit-learn计算MSE
+
+print ("MSE:",metrics.mean_squared_error(y_test, y_pred))
+# 用scikit-learn计算RMSE
+print ("RMSE:",np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
 
 
+from sklearn.neural_network import MLPRegressor
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Assuming you have already generated X and y using make_regression
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+# Initialize the scaler
+scaler_X = StandardScaler()
+scaler_y = StandardScaler()
+
+# Fit on the training data and transform both training and test data
+X_train_scaled = scaler_X.fit_transform(X_train)
+X_test_scaled = scaler_X.transform(X_test)
+
+# Fit on the training targets and transform both training and test targets
+y_train_scaled = scaler_y.fit_transform(y_train.values.reshape(-1, 1)).ravel()
+y_test_scaled = scaler_y.transform(y_test.values.reshape(-1, 1)).ravel()
 
 
+# Fit the regressor on the scaled data
+regr = MLPRegressor(random_state=1, max_iter=500).fit(X_train_scaled, y_train_scaled)
 
+# Make predictions on the scaled test data
+predictions_scaled = regr.predict(X_test_scaled)
+
+#  original scale, you can inverse transform the predictions
+predictions = scaler_y.inverse_transform(predictions_scaled.reshape(-1, 1)).ravel()
+
+
+# Print the results
+print(predictions_scaled)
+print(predictions)
+print(regr.score(X_test_scaled, y_test_scaled))
+
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y_test, predictions)
+print(f"Mean Squared Error: {mse}")
+print(f"RMSE: {mse** 0.5}")
+
+# compute the residuals:
+#predictions = regr.predict(X_test_scaled)
+#residuals = y_test_scaled - predictions
+
+# 1. Residuals vs. Fitted values plot
+#plt.figure(figsize=(8, 6))
+#plt.scatter(predictions, residuals)
+#plt.axhline(y=0, color='r', linestyle='--')
+#plt.xlabel("Fitted values")
+#plt.ylabel("Residuals")
+#plt.title("Residuals vs. Fitted Values")
+#plt.show()
+
+# 2. Histogram of Residuals
+#plt.figure(figsize=(8, 6))
+#plt.hist(residuals, bins=30, edgecolor='k')
+#plt.xlabel("Residual")
+#plt.ylabel("Frequency")
+#plt.title("Histogram of Residuals")
+#plt.show()
+
+#Prediction On China Economic Data
+df4 = pd.read_csv(r'C:\Users\jense\OneDrive\Desktop\Meta数据分析\ChinaEconomicData.csv')
+
+# Assuming new_data is your new dataset
+new_X = df4[['GDP_USD', 'GDP_per_capita_USD', 'hdi', 'population_density_km2', 'area_km2', 'population_million']]
+
+# Scale the new data using the same scaler object you used for the training data
+new_X_scaled = scaler_X.transform(new_X)
+
+# Use the trained model to make predictions
+new_predictions_scaled = regr.predict(new_X_scaled)
+
+# Convert the scaled predictions back to original scale if needed
+new_predictions = scaler_y.inverse_transform(new_predictions_scaled.reshape(-1, 1)).ravel()
+
+print("China's margin is:",new_predictions)
 
 
